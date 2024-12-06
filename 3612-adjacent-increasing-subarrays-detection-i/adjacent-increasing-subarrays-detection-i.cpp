8fe3d9792ed71d44234e24e30d203f5ace66c687
Namrata Mali <107704065+Namrata103@users.noreply.github.com>
2024-12-06 23:45:31 +0530

    Time: 21 ms (14.42%) | Memory: 40.4 MB (20.43%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
@@ -1,44 +1,23 @@
-// class Solution {
-// public:
-
-//     bool isStrictlyIncreasing(vector<int>& nums, int start, int k) {
-//         for(int i = start; i < start+k-1; i++){
-//             if(nums[i]>=nums[i+1]) return false;
-//         }
-//         return true;
-//     }
-
-//     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
-//         int n = nums.size();
-
-//         if(n<k*2) return false;
-//         if(k==1) return true;
-//         for(int i = 0; i <= n-k*2; i++){
-//             if(isStrictlyIncreasing(nums,i,k) && isStrictlyIncreasing(nums,i+k,k)){
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-// };
-
 class Solution {
- public:
-  bool hasIncreasingSubarrays(vector<int>& nums, int k) {
-    int increasing = 1;
-    int prevIncreasing = 0;
+public:
 
-    for (int i = 1; i < nums.size(); ++i) {
-      if (nums[i] > nums[i - 1]) {
-        ++increasing;
-      } else {
-        prevIncreasing = increasing;
-        increasing = 1;
-      }
-      if (increasing / 2 >= k || min(prevIncreasing, increasing) >= k)
+    bool isStrictlyIncreasing(vector<int>& nums, int start, int k) {
+        for(int i = start; i < start+k-1; i++){
+            if(nums[i]>=nums[i+1]) return false;
+        }
         return true;
     }
 
-    return false;
-  }
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        if(n<k*2) return false;
+        if(k==1) return true;
+        for(int i = 0; i <= n-k*2; i++){
+            if(isStrictlyIncreasing(nums,i,k) && isStrictlyIncreasing(nums,i+k,k)){
+                return true;
+            }
+        }
+        return false;
+    }
 };
