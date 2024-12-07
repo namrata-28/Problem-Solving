2b80a030ad961e10015d868cfca2c795b9d322d5
Namrata Mali <107704065+Namrata103@users.noreply.github.com>
2024-12-07 10:59:05 +0530

    Time: 4 ms (97.29%) | Memory: 40.5 MB (13.83%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
@@ -28,6 +28,7 @@ class Solution {
     int increasing = 1;
     int prevIncreasing = 0;
 
+    if(k==1) return true;
     for (int i = 1; i < nums.size(); ++i) {
       if (nums[i] > nums[i - 1]) {
         ++increasing;
