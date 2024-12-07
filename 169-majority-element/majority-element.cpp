c3474a37a0c817addad666364010739f5bc3e43e
Namrata Mali <107704065+Namrata103@users.noreply.github.com>
2024-12-07 12:46:20 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (42.23%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -10,23 +10,39 @@
 // TC:O(nlogn)
 
 //Moore's Voting Algorithm
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int majority,count=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(count)
+//             {
+//                 count+=(nums[i]==majority ? 1 : -1);
+//             }
+//             else
+//             {
+//                 majority=nums[i];
+//             }
+//         }
+//         return majority;
+//     }
+// };
+// TC:O(n)
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int majority,count=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(count)
-            {
-                count+=(nums[i]==majority ? 1 : -1);
+        int count = 0;
+        int candidate;
+
+        for (int& num : nums) {
+            if (count == 0) {
+                candidate = num;
             }
-            else
-            {
-                majority=nums[i];
-                count=1;
-            }
+            count += (num == candidate) ? 1 : -1;
         }
-        return majority;
+
+        return candidate;
     }
 };
-// TC:O(n)
