f9d44b3bf1d4842f006c252267dd686afe1ab204
Namrata Mali <107704065+Namrata103@users.noreply.github.com>
2024-12-07 22:27:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (6.02%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,6 +1,27 @@
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         return haystack.find(needle);
+//     }
+// };
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        return haystack.find(needle);
+        int m = needle.length();
+        int n = haystack.length();
+
+        for (int windowStart = 0; windowStart <= n - m; windowStart++) {
+            for (int i = 0; i < m; i++) {
+                if (needle[i] != haystack[windowStart + i]) {
+                    break;
+                }
+                if (i == m - 1) {
+                    return windowStart;
+                }
+            }
+        }
+
+        return -1;
     }
 };
