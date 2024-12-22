825c6acb00276a32d0d49909d5cfe4be31f8779b
Namrata Mali <107704065+Namrata103@users.noreply.github.com>
2024-12-22 15:47:32 +0530

    Time: 33 ms (86.22%) | Memory: 74.9 MB (79.81%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-      unordered_map<int,int> basket;
-      int left = 0, maxPicked = 0;
-
-      for(int right = 0; right < fruits.size(); right++) {
-        basket[fruits[right]]++;
-
-        while(basket.size() > 2) {
-            basket[fruits[left]]--;
-            if(basket[fruits[left]] == 0)
-                basket.erase(fruits[left]);
-            left++;
+        unordered_map<int, int> basket;
+        int left, right;
+        
+        for (left = 0, right = 0; right < fruits.size(); right++) {
+            basket[fruits[right]]++;
+            
+            if (basket.size() > 2) {
+                basket[fruits[left]]--;
+                if (basket[fruits[left]] == 0)
+                    basket.erase(fruits[left]);
+                left++;
+            }
         }
-
-        int windowSize = right - left + 1;
-        maxPicked = max(maxPicked, windowSize);
-      }
-        return maxPicked;
+        
+        return right - left;
     }
 };
