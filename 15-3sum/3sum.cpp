ce480f34033b9935d7a5c83cfa62d565f84c3402
Namrata Mali <107704065+Namrata103@users.noreply.github.com>
2025-01-16 23:02:20 +0530

    Time: 39 ms (95.50%) | Memory: 29.4 MB (45.34%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        for(int i=0; i<nums.size(); i++){
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
 
-            sort(nums.begin(), nums.end());
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int j = i+1;
-            int k = nums.size()-1;
-            while(j<k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum<0){
-                    j++;
-                } else if(sum>0){
-                    k--;
-                } else{
-                    vector <int> temp = {nums[i],nums[j],nums[k]};
-                    ans.push_back(temp);
-                    j++;
-                    k--;
-                    while(j<k && nums[j]==nums[j-1]) j++;
-                    while(j<k && nums[k]==nums[k+1]) k--;
-                }
+        for(int i = 0; i < nums.size() && nums[i] <= 0; i++) {
+            if(i == 0 || nums[i-1] != nums[i]) {
+                twoSumII(nums, i, res);
             }
         }
-        return ans;
+        return res;
+    }
+
+    void twoSumII(vector<int>& nums, int i, vector<vector<int>> &res) {
+        int lo = i+1, hi = nums.size()-1;
+
+        while(lo < hi) {
+            int sum = nums[i] + nums[lo] + nums[hi];
+
+            if(sum<0) {
+                ++lo;
+            } else if(sum>0) {
+                --hi;
+            } else {
+                res.push_back({nums[i], nums[lo++], nums[hi--]});
+                while(lo < hi && nums[lo] == nums[lo-1]) ++lo;
+            }
+        }
     }
 };
