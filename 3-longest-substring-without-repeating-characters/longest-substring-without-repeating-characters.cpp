ace47e517419b2434c99980e49e494e97f811900
Namrata Mali <107704065+Namrata103@users.noreply.github.com>
2025-01-16 20:01:43 +0530

    Time: 4 ms (77.34%) | Memory: 11.7 MB (69.51%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,16 +1,33 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         unordered_set<char> charSet;
+//         int l = 0;
+//         int res = 0;
+
+//         for (int r = 0; r < s.size(); r++) {
+//             while (charSet.find(s[r]) != charSet.end()) {
+//                 charSet.erase(s[l]);
+//                 l++;
+//             }
+//             charSet.insert(s[r]);
+//             res = max(res, r - l + 1);
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> charSet;
-        int l = 0;
-        int res = 0;
-
+        unordered_map<char, int> mp;
+        int l = 0, res = 0;
+        
         for (int r = 0; r < s.size(); r++) {
-            while (charSet.find(s[r]) != charSet.end()) {
-                charSet.erase(s[l]);
-                l++;
+            if (mp.find(s[r]) != mp.end()) {
+                l = max(mp[s[r]] + 1, l);
             }
-            charSet.insert(s[r]);
+            mp[s[r]] = r;
             res = max(res, r - l + 1);
         }
         return res;
