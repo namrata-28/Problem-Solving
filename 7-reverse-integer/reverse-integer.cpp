4940aafa95a9f1595db66769ce0a19c448909c76
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2025-05-05 09:08:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (97.60%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -5,9 +5,9 @@ public:
         while (x != 0) {
             int pop = x % 10;
             x /= 10;
-            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7))
+            if (rev > INT_MAX / 10)
                 return 0;
-            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8))
+            if (rev < INT_MIN / 10)
                 return 0;
             rev = rev * 10 + pop;
         }
