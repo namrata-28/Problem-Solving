8f351cc7dcfe5c9a28a8d6e7fb77627528d6c183
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2025-11-24 08:18:54 +0530

    Time: 0 ms (100.00%) | Memory: 63 MB (47.95%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,20 +1,40 @@
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         int firstContainer = 0, lastContainer = height.size()-1;
+//         int maxWater = 0;
+//         while(firstContainer<lastContainer){
+//             int smallHeight = (height[firstContainer] < height[lastContainer]) ? height[firstContainer] : height[lastContainer];
+//             int currentWater = smallHeight*(lastContainer-firstContainer);
+//             if(currentWater>maxWater){
+//                 maxWater = currentWater;
+//             }
+//             if(height[firstContainer]<=height[lastContainer]){
+//                 firstContainer++;
+//             } else{
+//                 lastContainer--;
+//             }
+//         }
+//         return maxWater;
+//     }
+// };
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int firstContainer = 0, lastContainer = height.size()-1;
-        int maxWater = 0;
-        while(firstContainer<lastContainer){
-            int smallHeight = (height[firstContainer] < height[lastContainer]) ? height[firstContainer] : height[lastContainer];
-            int currentWater = smallHeight*(lastContainer-firstContainer);
-            if(currentWater>maxWater){
-                maxWater = currentWater;
-            }
-            if(height[firstContainer]<=height[lastContainer]){
-                firstContainer++;
-            } else{
-                lastContainer--;
+        int maxArea = 0;
+        int left = 0;
+        int right = height.size() - 1;
+
+        while (left < right) {
+            int width = right - left;
+            maxArea = max(maxArea, min(height[left], height[right]) * width);
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
             }
         }
-        return maxWater;
+        return maxArea;
     }
 };
