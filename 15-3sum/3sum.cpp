f770ad259218efff70070b39dcfce6f67f802151
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2025-11-30 06:54:13 +0530

    Time: 1532 ms (5.00%) | Memory: 449.2 MB (5.01%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        sort(begin(nums), end(nums));
         vector<vector<int>> res;
-
-        for(int i = 0; i < nums.size() && nums[i] <= 0; i++) {
-            if(i == 0 || nums[i-1] != nums[i]) {
-                twoSumII(nums, i, res);
+        for (int i = 0; i < nums.size() && nums[i] <= 0; ++i)
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSum(nums, i, res);
             }
-        }
         return res;
     }
-
-    void twoSumII(vector<int>& nums, int i, vector<vector<int>> &res) {
-        int lo = i+1, hi = nums.size()-1;
-
-        while(lo < hi) {
-            int sum = nums[i] + nums[lo] + nums[hi];
-
-            if(sum<0) {
-                ++lo;
-            } else if(sum>0) {
-                --hi;
-            } else {
-                res.push_back({nums[i], nums[lo++], nums[hi--]});
-                while(lo < hi && nums[lo] == nums[lo-1]) ++lo;
+    void twoSum(vector<int>& nums, int i, vector<vector<int>>& res) {
+        unordered_set<int> seen;
+        for (int j = i + 1; j < nums.size(); ++j) {
+            int complement = -nums[i] - nums[j];
+            if (seen.count(complement)) {
+                res.push_back({nums[i], complement, nums[j]});
+                while (j + 1 < nums.size() && nums[j] == nums[j + 1]) {
+                    ++j;
+                }
             }
+            seen.insert(nums[j]);
         }
     }
 };
