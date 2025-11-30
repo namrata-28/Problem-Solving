b6ff1895a34b77f3cd1a0b3d9ac2b019a7a41356
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2025-11-30 07:18:06 +0530

    Time: 35 ms (97.44%) | Memory: 29.5 MB (35.17%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -3,23 +3,27 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(begin(nums), end(nums));
         vector<vector<int>> res;
-        for (int i = 0; i < nums.size() && nums[i] <= 0; ++i)
+        for (int i = 0; i < nums.size() && nums[i] <= 0; ++i) {
             if (i == 0 || nums[i - 1] != nums[i]) {
-                twoSum(nums, i, res);
+                twoSumII(nums, i, res);
             }
+        }
         return res;
     }
-    void twoSum(vector<int>& nums, int i, vector<vector<int>>& res) {
-        unordered_set<int> seen;
-        for (int j = i + 1; j < nums.size(); ++j) {
-            int complement = -nums[i] - nums[j];
-            if (seen.count(complement)) {
-                res.push_back({nums[i], complement, nums[j]});
-                while (j + 1 < nums.size() && nums[j] == nums[j + 1]) {
-                    ++j;
-                }
+    void twoSumII(vector<int>& nums, int i, vector<vector<int>>& res) {
+        int lo = i + 1, hi = nums.size() - 1;
+        while (lo < hi) {
+            int sum = nums[i] + nums[lo] + nums[hi];
+            if (sum < 0) {
+                ++lo;
+            } else if (sum > 0) {
+                --hi;
+            } else {
+                res.push_back({nums[i], nums[lo++], nums[hi--]});
+                // Skip duplicates on both sides to reduce redundant checks
+                while (lo < hi && nums[lo] == nums[lo - 1]) ++lo;
+                while (lo < hi && nums[hi] == nums[hi + 1]) --hi;
             }
-            seen.insert(nums[j]);
         }
     }
 };
