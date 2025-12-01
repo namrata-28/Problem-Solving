ac116c88091464015e3ef9d2a21f98c4d47eba29
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2025-12-01 22:33:19 +0530

    Time: 39 ms (93.67%) | Memory: 29.3 MB (36.30%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(begin(nums), end(nums));
-        vector<vector<int>> res;
-        for (int i = 0; i < nums.size() && nums[i] <= 0; ++i) {
-            if (i == 0 || nums[i - 1] != nums[i]) {
-                twoSumII(nums, i, res);
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+
+        for(int i = 0; i < nums.size() && nums[i] <= 0; i++){
+            if(i == 0 || nums[i] != nums[i-1]) {
+                twoSum(nums, i , ans);
             }
         }
-        return res;
+        return ans;
     }
-    void twoSumII(vector<int>& nums, int i, vector<vector<int>>& res) {
+
+    void twoSum(vector<int>& nums, int i, vector<vector<int>>& ans) {
         int lo = i + 1, hi = nums.size() - 1;
-        while (lo < hi) {
+        while(lo < hi) {
             int sum = nums[i] + nums[lo] + nums[hi];
-            if (sum < 0) {
-                ++lo;
-            } else if (sum > 0) {
-                --hi;
+            if(sum < 0 ){
+                lo++;
+            } else if(sum > 0) {
+                hi--;
             } else {
-                res.push_back({nums[i], nums[lo++], nums[hi--]});
-                // Skip duplicates on both sides to reduce redundant checks
-                while (lo < hi && nums[lo] == nums[lo - 1]) ++lo;
-                while (lo < hi && nums[hi] == nums[hi + 1]) --hi;
+                ans.push_back({nums[i], nums[lo], nums[hi]});
+                lo++;
+                hi--;
+                while(lo < hi && nums[lo] == nums[lo-1]) lo++;
+                while(lo < hi && nums[hi] == nums[hi+1]) hi--;
             }
         }
     }
