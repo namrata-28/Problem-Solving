102216fe127ea844cbd4a1a1bbd149670f4aff1b
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-02-02 09:35:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (74.25%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,20 +2,42 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-        int maxlen = 0;
+        vector<int> hash(256,-1);
 
-        for(int i = 0; i < n; i++) {
-            vector<int> hash(256,0);
-
-            for(int j = i; j < n; j++) {
-                if(hash[s[j]] == 1) break;
-
-                hash[s[j]] = 1;
-                int len = j - i + 1;
-
-                maxlen = max(maxlen,len);
+        int l = 0, r = 0, maxlen = 0;
+        while( r < n) {
+            if(hash[s[r]] != -1) {
+                l = max(hash[s[r]] + 1, l);
             }
+
+            int len = r - l + 1;
+            maxlen = max(maxlen,len);
+            hash[s[r]] = r;
+            r++;
         }
         return maxlen;
-    }
+    }  
 };
+
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int n = s.size();
+//         int maxlen = 0;
+
+//         for(int i = 0; i < n; i++) {
+//             vector<int> hash(256,0);
+
+//             for(int j = i; j < n; j++) {
+//                 if(hash[s[j]] == 1) break;
+
+//                 hash[s[j]] = 1;
+//                 int len = j - i + 1;
+
+//                 maxlen = max(maxlen,len);
+//             }
+//         }
+//         return maxlen;
+//     }
+// };
