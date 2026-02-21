7978b9eb1f166e56005feb16dc507796d6de6977
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-02-21 14:53:21 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (6.29%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,30 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int minprice = INT_MAX;
+//         int maxprofit = 0;
+//         for (int i = 0; i < prices.size(); i++) {
+//             if (prices[i] < minprice)
+//                 minprice = prices[i];
+//             else if (prices[i] - minprice > maxprofit)
+//                 maxprofit = prices[i] - minprice;
+//         }
+//         return maxprofit;
+//     }
+// };
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minprice = INT_MAX;
-        int maxprofit = 0;
-        for (int i = 0; i < prices.size(); i++) {
-            if (prices[i] < minprice)
-                minprice = prices[i];
-            else if (prices[i] - minprice > maxprofit)
-                maxprofit = prices[i] - minprice;
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+        for(int i = 0; i < prices.size(); i++) {
+            if(prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if(prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
         }
-        return maxprofit;
+        return maxProfit;
     }
 };
