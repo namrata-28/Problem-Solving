62dd6bac5b0e8aec8d06b6b524a38bf2321501f1
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-03-01 14:20:03 +0530

    Time: 19 ms (27.76%) | Memory: 14.5 MB (13.38%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,51 @@
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        vector<int> hash(256,-1);
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int n = s.size();
+//         vector<int> hash(256,-1);
 
-        int l = 0, r = 0, maxlen = 0;
-        while( r < n) {
-            if(hash[s[r]] != -1) {
-                l = max(hash[s[r]] + 1, l);
-            }
+//         int l = 0, r = 0, maxlen = 0;
+//         while( r < n) {
+//             if(hash[s[r]] != -1) {
+//                 l = max(hash[s[r]] + 1, l);
+//             }
 
-            int len = r - l + 1;
-            maxlen = max(maxlen,len);
-            hash[s[r]] = r;
-            r++;
-        }
-        return maxlen;
-    }  
-};
+//             int len = r - l + 1;
+//             maxlen = max(maxlen,len);
+//             hash[s[r]] = r;
+//             r++;
+//         }
+//         return maxlen;
+//     }  
+// };
 
+// My bruteforce
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int size = s.size();
+//         int maxlen = 0;
+//         unordered_set<char> uniqueChars;
+//         for(int i = 0; i < size; i++) {
+//             int count = 0;
+//             for(int j = i ; j < size; j++) {
+//                 if(uniqueChars.find(s[j])==uniqueChars.end()) {
+//                     uniqueChars.insert(s[j]);
+//                     count++;
+//                 } else {
+//                     break;
+//                 }
+//             }
+//             maxlen = max(maxlen,count);
+//             uniqueChars.clear();
+//         }
+//         return maxlen;
+//     }
+// };
+
+
+// Striver's brute force
 
 // class Solution {
 // public:
@@ -41,3 +68,26 @@ public:
 //         return maxlen;
 //     }
 // };
+
+
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int size = s.size();
+        int maxlen = 0;
+        int left = 0;
+
+        unordered_set<char> uniqueChars;
+        for(int right = 0; right < size; right++) {
+            while(uniqueChars.find(s[right]) != uniqueChars.end()) {
+                uniqueChars.erase(s[left]);
+                left++;
+            }
+
+            uniqueChars.insert(s[right]);
+            maxlen = max(maxlen, right-left+1);
+        }
+        return maxlen;
+    }
+};
