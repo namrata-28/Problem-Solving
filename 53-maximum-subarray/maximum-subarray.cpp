ff0831ad5cb08d3d4f7605c77c3dddbc380a237b
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-03-01 09:28:47 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (15.20%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,30 +1,37 @@
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//     long long prefix = 0;
+//     long long minPrefix = 0;   // minimum prefix seen so far
+//     long long maxSum = LLONG_MIN;
+
+//     for (int num : nums) {
+//         prefix += num;
+
+//         // Maximum subarray ending here
+//         maxSum = max(maxSum, prefix - minPrefix);
+
+//         // Update minimum prefix
+//         minPrefix = min(minPrefix, prefix);
+//     }
+
+//     return (int)maxSum;
+//     }
+// };
+
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-    //         long long prefix = 0;
-    // long long minPrefix = 0;   // minimum prefix seen so far
-    // long long maxSum = LLONG_MIN;
+        int numsSize = nums.size();
+        int currSum = nums[0];
+        int maxSum = nums[0];
 
-    // for (int num : nums) {
-    //     prefix += num;
+        for(int i = 1; i < numsSize; i++) {
+            currSum = max(currSum+nums[i], nums[i]);
+            maxSum = max(maxSum, currSum);
+        }
 
-    //     // Maximum subarray ending here
-    //     maxSum = max(maxSum, prefix - minPrefix);
-
-    //     // Update minimum prefix
-    //     minPrefix = min(minPrefix, prefix);
-    // }
-
-    // return (int)maxSum;
-    // }
-    int currSum = nums[0];
-    int maxSum = nums[0];
-
-    for(int i = 1; i < nums.size(); i++){
-        currSum = max(nums[i], currSum + nums[i]);
-        maxSum = max(maxSum, currSum);
-    }
-    
-    return maxSum;
+        return maxSum;
     }
 };
