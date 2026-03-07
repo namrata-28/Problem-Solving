6b883d5c654a584adeaf61e7eb3317fbfe5a11d2
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-03-07 14:21:33 +0530

    Time: 3 ms (35.12%) | Memory: 63 MB (48.06%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,38 +1,16 @@
-// class Solution {
-// public:
-//     int maxArea(vector<int>& height) {
-//         int firstContainer = 0, lastContainer = height.size()-1;
-//         int maxWater = 0;
-//         while(firstContainer<lastContainer){
-//             int smallHeight = (height[firstContainer] < height[lastContainer]) ? height[firstContainer] : height[lastContainer];
-//             int currentWater = smallHeight*(lastContainer-firstContainer);
-//             if(currentWater>maxWater){
-//                 maxWater = currentWater;
-//             }
-//             if(height[firstContainer]<=height[lastContainer]){
-//                 firstContainer++;
-//             } else{
-//                 lastContainer--;
-//             }
-//         }
-//         return maxWater;
-//     }
-// };
-
 class Solution {
 public:
     int maxArea(vector<int>& height) {
         int maxArea = 0;
-        int left = 0;
-        int right = height.size() - 1;
+        int l = 0, r = height.size()-1;
 
-        while (left < right) {
-            int width = right - left;
-            maxArea = max(maxArea, min(height[left], height[right]) * width);
-            if (height[left] <= height[right]) {
-                left++;
+        while(l < r) {
+            int width = r - l;
+            maxArea = max(maxArea, min(height[l],height[r]) * width);
+            if(height[l] < height[r]) {
+                l++;
             } else {
-                right--;
+                r--;
             }
         }
         return maxArea;
