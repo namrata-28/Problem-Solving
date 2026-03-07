a3567ff09d3ada4297477df1c9b726a711796691
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-03-07 15:13:00 +0530

    Time: 47 ms (66.89%) | Memory: 28.9 MB (91.00%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
         vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
 
-        for(int i=0;i<nums.size();i++){
+        sort(nums.begin(),nums.end());
 
-            if(i>0 && nums[i]==nums[i-1])
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(i>0 && nums[i]==nums[i-1]) {
                 continue;
+            }
 
-            int l=i+1;
-            int r=nums.size()-1;
+            //2 pointers
 
-            while(l<r){
+            int l = i+1;
+            int r=n-1;
 
+            while(l<r) {
                 int sum = nums[i]+nums[l]+nums[r];
 
-                if(sum==0){
-
+                if(sum < 0) {
+                    l++;
+                } else if(sum > 0) {
+                    r--;
+                } else{
+                    //sum = 0;
                     ans.push_back({nums[i],nums[l],nums[r]});
-
-                    while(l<r && nums[l]==nums[l+1]) l++;
-                    while(l<r && nums[r]==nums[r-1]) r--;
-
                     l++;
                     r--;
+
+                    while(l<r && nums[l] == nums[l-1]) l++;
+                    while(l<r && nums[r] == nums[r+1]) r--;
                 }
-
-                else if(sum<0)
-                    l++;
-
-                else
-                    r--;
             }
         }
 
