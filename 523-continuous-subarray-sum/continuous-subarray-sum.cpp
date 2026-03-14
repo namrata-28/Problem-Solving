c6f2469d3d49a77858c341ded7c8e54fab61163d
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-03-14 09:32:57 +0530

    Time: 75 ms (78.54%) | Memory: 146.4 MB (23.59%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int sum = 0;
+        unordered_map <int,int> mp;
+        mp[0] = -1;
+
+        for(int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+
+            int rem = sum % k;
+            if(rem == 0 && i > 0) return true;
+
+            if(mp.count(rem) && i - mp[rem] >= 2 ) {
+                return true;
+            }
+            
+            if(!mp.count(rem)) {
+                mp[rem] = i;
+            }
+
+        }
+
+        return false;
+    }
+};
