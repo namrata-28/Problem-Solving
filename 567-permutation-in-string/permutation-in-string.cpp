43577b27931c6ccaf3efc10d91e34ca2fb0d3b20
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-03-14 13:22:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (88.46%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n1 = s1.size();
+        int n2 = s2.size();
+
+        if(n1 > n2) return false;
+
+        vector<int> f1(26,0);
+        vector<int> f2(26,0);
+
+        for(int i = 0; i < n1; i++){
+            f1[s1[i]-'a']++;
+            f2[s2[i]-'a']++;
+        }
+
+        if(f1 == f2) return true;
+
+        for(int i = n1; i < n2; i++){
+            f2[s2[i]-'a']++;
+            f2[s2[i-n1]-'a']--;
+
+            if(f1 == f2) return true;
+        }
+
+        return false;
+    }
+};
