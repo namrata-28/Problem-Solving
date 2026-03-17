d4a8a1c25c0c52def895fd90402f9306d4598fe0
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-03-17 20:02:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (100.00%) - LeetSync

--- 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,45 +1,45 @@
-// class Solution {
-// public:
-//     int minStartValue(vector<int>& nums) {
-//         int n = nums.size();
-//         int minPrefixSum = INT_MAX;
-//         int sum = 0;
-
-//         for(int num : nums) {
-//             sum += num;
-//             minPrefixSum = min(minPrefixSum, sum);
-//         }
-
-//         if(minPrefixSum >= 1)
-//         {
-//             return 1;
-//         } else {
-//             return abs(minPrefixSum) + 1;
-//         }
-//     }
-// };
-
-
 class Solution {
 public:
     int minStartValue(vector<int>& nums) {
-        // We use "total" for current step-by-step total, "minVal" for minimum 
-        // step-by-step total among all sums. Since we always start with 
-        // startValue = 0, therefore the initial current step-by-step total is 0, 
-        // thus we set "total" and "minVal" be 0. 
-        int minVal = 0;
-        int total = 0;
+        int n = nums.size();
+        int minPrefixSum = INT_MAX;
+        int sum = 0;
 
-        // Iterate over the array and get the minimum step-by-step total.
-        for (int num : nums) {
-
-            total += num;
-            minVal = min(minVal, total);
+        for(int num : nums) {
+            sum += num;
+            minPrefixSum = min(minPrefixSum, sum);
         }
-        
-        // We have to let the minimum step-by-step total equals to 1, 
-        // by increasing the startValue from 0 to -minVal + 1, 
-        // which is just the minimum startValue we want.
-        return -minVal + 1;
+
+        if(minPrefixSum >= 1)
+        {
+            return 1;
+        } else {
+            return 1 - minPrefixSum;
+        }
     }
 };
+
+
+// class Solution {
+// public:
+//     int minStartValue(vector<int>& nums) {
+//         // We use "total" for current step-by-step total, "minVal" for minimum 
+//         // step-by-step total among all sums. Since we always start with 
+//         // startValue = 0, therefore the initial current step-by-step total is 0, 
+//         // thus we set "total" and "minVal" be 0. 
+//         int minVal = 0;
+//         int total = 0;
+
+//         // Iterate over the array and get the minimum step-by-step total.
+//         for (int num : nums) {
+
+//             total += num;
+//             minVal = min(minVal, total);
+//         }
+        
+//         // We have to let the minimum step-by-step total equals to 1, 
+//         // by increasing the startValue from 0 to -minVal + 1, 
+//         // which is just the minimum startValue we want.
+//         return -minVal + 1;
+//     }
+// };
