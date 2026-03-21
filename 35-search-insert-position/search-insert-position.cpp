9cec467553280c3616b69c38470336a8d57f7dee
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-03-21 12:04:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (42.65%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -6,9 +6,9 @@ public:
             int mid = l + (r - l)/2;
             if(target == nums[mid]) return mid;
             else if(target < nums[mid]) {
-                r--;
+                r = mid-1;
             } else {
-                l++;
+                l = mid+1;
             }
         }
         return l;
