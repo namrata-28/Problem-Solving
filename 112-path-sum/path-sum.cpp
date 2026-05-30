2b121ff3f3b95f543c08e7cf3461ce5c4fd3096a
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-05-30 11:40:43 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (65.65%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -14,7 +14,7 @@ public:
     bool hasPathSum(TreeNode* root, int sum) {
         if (!root) return false;
         sum -= root->val;
-        if (!root->left && !root->right)  // if reach a leaf
+        if (!root->left && !root->right)
             return sum == 0;
         return hasPathSum(root->left, sum) || hasPathSum(root->right, sum);
     }
