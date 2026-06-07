d237f5465e5bb1e914f0733bb5a19be53f7d5c76
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-06-07 11:57:10 +0530

    Time: 34 ms (50.85%) | Memory: 61.5 MB (50.81%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,14 +1,33 @@
+// max heap = n log n
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int> pq;
+
+//         for(int num : nums) {
+//             pq.push(num);
+//         }
+
+//         for(int i = 0; i < k-1; i++) {
+//             pq.pop();
+//         }
+
+//         return pq.top();
+//     }
+// };
+
+// min heap = n log k
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq;
+        priority_queue<int, vector<int>, greater<int>> pq;
 
         for(int num : nums) {
             pq.push(num);
-        }
 
-        for(int i = 0; i < k-1; i++) {
-            pq.pop();
+            if(pq.size() > k) {
+                pq.pop();
+            }
         }
 
         return pq.top();
