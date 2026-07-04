8b705b83e75820eddb5d2d93bf88a18e93ed70ea
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-07-04 11:40:12 +0530

    Time: 1 ms (26.69%) | Memory: 9.4 MB (29.67%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-   int uniquePaths(int m, int n) {
-        int N = n+m-2;
-        int r = m-1;
-        double res = 1;
-        
-        for(int i=1; i<=r; i++){
-            res = res * ( N - r + i) / i;
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> d(m, vector<int>(n, 1));
+
+        for (int col = 1; col < m; ++col) {
+            for (int row = 1; row < n; ++row) {
+                d[col][row] = d[col - 1][row] + d[col][row - 1];
+            }
         }
-        
-        return (int)res;
+
+        return d[m - 1][n - 1];
     }
 };
