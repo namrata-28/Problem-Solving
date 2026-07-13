0dc4dd50662b13147c24645054b2433623cf7e71
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-07-13 22:03:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (73.22%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -4,16 +4,19 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int left = 1;
-        int right = n;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (isBadVersion(mid)) {
-                right = mid;
+        int first = 1;
+        int last = n;
+        int ans;
+
+        while(first <= last) {
+            int mid = first + (last - first)/2;
+            if(isBadVersion(mid)) {
+                ans = mid;
+                last = mid - 1;
             } else {
-                left = mid + 1;
+                first = mid + 1;
             }
         }
-        return left;
+        return ans;
     }
 };
