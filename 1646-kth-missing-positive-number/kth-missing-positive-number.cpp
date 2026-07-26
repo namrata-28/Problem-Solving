73c5ea08420987b1ae9e79584dac35c6ad08016d
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-07-26 10:59:17 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (61.20%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,13 +1,31 @@
+// class Solution {
+// public:
+//     int findKthPositive(vector<int>& arr, int k) {
+//         for(int num : arr){
+//             if(num <= k){
+//                 k++;
+//             } else{
+//                 break;
+//             }
+//         }
+//         return k;
+//     }
+// };
+
+//  bs +  math tricky
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        for(int num : arr){
-            if(num <= k){
-                k++;
-            } else{
-                break;
+        int low = 0, high = arr.size()-1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            int missing = arr[mid] - (mid + 1);
+            if(missing < k){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
             }
         }
-        return k;
+        return low + k;
     }
 };
