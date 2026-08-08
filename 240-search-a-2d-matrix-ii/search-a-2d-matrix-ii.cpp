e49d2261d3aa1832555e24ef34c6ca31ebf0244e
Namrata Mali <107704065+namrata-28@users.noreply.github.com>
2026-08-08 14:12:30 +0530

    Time: 55 ms (51.36%) | Memory: 18.7 MB (69.20%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,23 +1,42 @@
+// class Solution {
+// public:
+//     bool binarySearch(vector<int> &arr, int target){
+//         int low = 0, high = arr.size()-1;
+//         while(low<=high){
+//             int mid = low + (high-low)/2;
+//             if(target == arr[mid]) return true;
+//             else if(target < arr[mid]) high = mid - 1;
+//             else  low= mid + 1;
+//         }
+//         return false;
+//     }
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+//         for(int i = 0; i < m; i++){
+//             if(matrix[i][0]<=target && target <= matrix[i][n-1]){
+//                 if(binarySearch(matrix[i], target)) 
+//                     return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
-    bool binarySearch(vector<int> &arr, int target){
-        int low = 0, high = arr.size()-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(target == arr[mid]) return true;
-            else if(target < arr[mid]) high = mid - 1;
-            else  low= mid + 1;
-        }
-        return false;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
-        for(int i = 0; i < m; i++){
-            if(matrix[i][0]<=target && target <= matrix[i][n-1]){
-                if(binarySearch(matrix[i], target)) 
-                    return true;
-            }
+
+        int row = 0, col = n-1;
+        while(row < m && col >= 0){
+            int currElem = matrix[row][col];
+
+            if(currElem == target) return true;
+            else if(currElem < target) row++;
+            else col --;
         }
         return false;
     }
